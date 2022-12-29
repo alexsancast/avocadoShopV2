d4c3dbf77150019f3496c8d375a0e3a4d9bb63db
Alexander <alexsancast@outlook.com>
2022-12-29 17:11:20 -0400

    Ultimo commit desde el trabajo

--- src/components/Boxprofile.jsx
+++ src/components/Boxprofile.jsx
@@ -4,12 +4,21 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 export default function Boxprofile() {
-  const { loginWithRedirect, logout } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
   return (
     <div className="container__boxprofile">
-      <NavLink to="/profile">Profile</NavLink>
-      <button onClick={loginWithRedirect}>Login</button>
-      <button onClick={logout}>Logout</button>
+      {isAuthenticated ? (
+        <>
+          <NavLink to="/profile">Profile</NavLink>
+          <button onClick={logout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <button onClick={loginWithRedirect}>Sign in </button>
+
+          <h1>New customer? </h1>
+        </>
+      )}
     </div>
   );
 }
